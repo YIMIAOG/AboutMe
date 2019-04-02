2c70f1b98fe9cac50b5e4b7a24c4a97685b99768
Soft Dev <softdev@robotics-mba-07.local>
2019-04-02 15:50:16 -0400

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Soft Dev on 4/2/19.
+//  Copyright © 2019 Lily Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
